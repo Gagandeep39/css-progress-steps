2c60e6388aa2bb0cd74512ab2709bc17ef18abd1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-21 19:19:54 +0530

    Added comments

--- script.js
+++ script.js
@@ -17,15 +17,18 @@ prev.addEventListener('click', () => {
 });
 
 function update() {
+  // Addeding active class to nnumber of circle bsed in current active count
   circles.forEach((circle, index) => {
     if (index < currentActive) circle.classList.add('active');
     else circle.classList.remove('active');
   });
 
+  // Calculates the width to be highlighted
   const actives = document.querySelectorAll('.active');
   progress.style.width =
     ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
 
+  // Hihglighing/dusabling bbuttons
   if (currentActive === 1) prev.disabled = true;
   else if (currentActive === circles.length) next.disabled = true;
   else {
